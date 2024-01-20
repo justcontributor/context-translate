4174851db5c0245a557e91554043d14923bb3154
Coding-Young <dumkty5663@gmail.com>
2024-01-20 18:57:36 +0900

    Attempted to embed modules into each script

--- background.js
+++ background.js
@@ -1,9 +1,67 @@
-import { translate } from "./module.js";
+//Module START
+const getSelectionText = () => {
+  if (window.getSelection) {
+    return window.getSelection().toString();
+  }
+  if (document.selection && document.selection.type != "Control") {
+    return document.selection.createRange().text;
+  }
+};
+
+const copy = (text) => {
+  const el = document.createElement("textarea");
+  el.value = text;
+  el.setAttribute("readonly", "");
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+};
+
+const openSidebar = (url, q, andEscape = "%26") => {
+  const tar = url + encodeURI(q).replaceAll("&", andEscape);
+  window.open(tar, "_blank", "whale-sidebar");
+};
+
+const translate = (arg) => {
+  const url = {
+    google: "https://translate.google.com/?sl=auto&tl=ko&text=",
+    papago: "https://papago.naver.com/?sk=auto&tk=ko&st=",
+    kakaoi: "https://translate.kakao.com/?q=",
+    bing: "https://www.bing.com/translator?from=auto&to=ko&text=",
+    set: "https://translators.to/?source=&target=ko&text=",
+    yandex: "https://translate.yandex.com/?lang=en-ko&text=",
+    flitto:
+      "https://ko.flitto.com/language/translation/text?src_lang_id=17&dst_lang_id=33&lang_id=33&q=",
+    samsung: "https://translate.samsung.com/?text=",
+    deepl: "https://www.deepl.com/translator#auto/ko/",
+  };
+
+  const andEscape = {
+    papago: "%25amp",
+  };
+
+  whale.storage.sync.get(
+    {
+      Service: "papago",
+      Copy: 0,
+    },
+    (items) => {
+      let q = arg ? arg : getSelectionText();
+      if (items.Copy) copy(q);
+      let service = items.Service;
+      openSidebar(url[service], q, andEscape[service]);
+    }
+  );
+};
+// Module END
 
 chrome.runtime.onInstalled.addListener((object) => {
   chrome.contextMenus.create({
     title: "우클릭 번역",
-    id: "id",
+    id: "contextTranslate",
     contexts: ["selection"],
   });
 
@@ -13,12 +71,6 @@ chrome.runtime.onInstalled.addListener((object) => {
   }
 });
 
-chrome.contextMenus.onClicked.addListener((info, tab) => {
-  if (!tab) return false;
-  if (tab.id < 0) {
-    // window.open("https://naver.com", "_blank", "whale-sidebar");
-    // translate(info.selectionText);
-  } else {
-    chrome.tabs.sendMessage(tab.id, { arg: null });
-  }
+chrome.contextMenus.onClicked.addListener((info) => {
+  translate(info.selectionText);
 });
--- content.js
+++ content.js
@@ -1,3 +1,63 @@
+//Module START
+const getSelectionText = () => {
+  if (window.getSelection) {
+    return window.getSelection().toString();
+  }
+  if (document.selection && document.selection.type != "Control") {
+    return document.selection.createRange().text;
+  }
+};
+
+const copy = (text) => {
+  const el = document.createElement("textarea");
+  el.value = text;
+  el.setAttribute("readonly", "");
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+};
+
+const openSidebar = (url, q, andEscape = "%26") => {
+  const tar = url + encodeURI(q).replaceAll("&", andEscape);
+  window.open(tar, "_blank", "whale-sidebar");
+};
+
+const translate = (arg) => {
+  const url = {
+    google: "https://translate.google.com/?sl=auto&tl=ko&text=",
+    papago: "https://papago.naver.com/?sk=auto&tk=ko&st=",
+    kakaoi: "https://translate.kakao.com/?q=",
+    bing: "https://www.bing.com/translator?from=auto&to=ko&text=",
+    set: "https://translators.to/?source=&target=ko&text=",
+    yandex: "https://translate.yandex.com/?lang=en-ko&text=",
+    flitto:
+      "https://ko.flitto.com/language/translation/text?src_lang_id=17&dst_lang_id=33&lang_id=33&q=",
+    samsung: "https://translate.samsung.com/?text=",
+    deepl: "https://www.deepl.com/translator#auto/ko/",
+  };
+
+  const andEscape = {
+    papago: "%25amp",
+  };
+
+  whale.storage.sync.get(
+    {
+      Service: "papago",
+      Copy: 0,
+    },
+    (items) => {
+      let q = arg ? arg : getSelectionText();
+      if (items.Copy) copy(q);
+      let service = items.Service;
+      openSidebar(url[service], q, andEscape[service]);
+    }
+  );
+};
+// Module END
+
 whale.storage.sync.get(
   {
     Key: "F8",
@@ -15,15 +75,15 @@ whale.storage.sync.get(
   }
 );
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) =>
-  sendResponse(
-    (async () => {
-      const src = chrome.runtime.getURL("module.js");
-      const script = await import(src);
-      script.translate(request.arg);
-    })()
-  )
-);
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) =>
+//   sendResponse(
+//     (async () => {
+//       const src = chrome.runtime.getURL("module.js");
+//       const script = await import(src);
+//       script.translate(request.arg);
+//     })()
+//   )
+// );
 
 window.onkeyup = (e) => {
   if (
@@ -34,10 +94,6 @@ window.onkeyup = (e) => {
     e.altKey == alt
   ) {
     e.preventDefault();
-    (async () => {
-      const src = chrome.runtime.getURL("module.js");
-      const script = await import(src);
-      script.translate();
-    })();
+    translate();
   }
 };
