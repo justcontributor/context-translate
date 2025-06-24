a7740f8ef3e28830a686bd36822c9a19de1ff2ea
justcontributor <dumkty5663@gmail.com>
2025-06-24 15:37:56 +0900

    refactor

--- module.js
+++ module.js
@@ -1,57 +1,60 @@
 const getSelectionText = () => {
-  if (window.getSelection) {
-    return window.getSelection().toString();
-  }
-  if (document.selection && document.selection.type != "Control") {
+  if (window.getSelection) return window.getSelection().toString();
+  if (document.selection && document.selection.type !== "Control")
     return document.selection.createRange().text;
-  }
+  return "";
 };
 
 const copy = (text) => {
   const el = document.createElement("textarea");
   el.value = text;
   el.setAttribute("readonly", "");
-  el.style.cssText = "position: absolute; left: -9999px";
+  el.style.position = "absolute";
+  el.style.left = "-9999px";
   document.body.appendChild(el);
   el.select();
-  document.execCommand("copy"); // new API requests permission too frequently
+  document.execCommand("copy");
   document.body.removeChild(el);
 };
 
-const openSidebar = (url, q, andEscape = "%26") => {
-  const tar = url + encodeURI(q).replaceAll("&", andEscape);
+const escapeURL = (service, q) => {
+  return (
+    url[service] + encodeURI(q).replaceAll("&", andEscape[service] || "%26")
+  );
+};
+
+const openSidebar = (tar) => {
   window.open(tar, "_blank", "whale-sidebar");
 };
 
+const url = {
+  google: "https://translate.google.com/?sl=auto&tl=ko&text=",
+  papago: "https://papago.naver.com/?sk=auto&tk=ko&st=",
+  kakaoi: "https://translate.kakao.com/?q=",
+  bing: "https://www.bing.com/translator?from=auto&to=ko&text=",
+  set: "https://papago.naver.com/?sk=auto&tk=ko&st=", // 번역기들 fallback
+  yandex: "https://translate.yandex.com/?lang=en-ko&text=",
+  flitto:
+    "https://ko.flitto.com/language/translation/text?src_lang_id=17&dst_lang_id=33&lang_id=33&q=",
+  samsung: "https://translate.samsung.com/?text=",
+  deepl: "https://www.deepl.com/translator#auto/ko/",
+};
+
+const andEscape = {
+  papago: "%25amp",
+};
+
 const translate = (arg) => {
-  const url = {
-    google: "https://translate.google.com/?sl=auto&tl=ko&text=",
-    papago: "https://papago.naver.com/?sk=auto&tk=ko&st=",
-    kakaoi: "https://translate.kakao.com/?q=",
-    bing: "https://www.bing.com/translator?from=auto&to=ko&text=",
-    set: "https://papago.naver.com/?sk=auto&tk=ko&st=", // 번역기들 fallback
-    yandex: "https://translate.yandex.com/?lang=en-ko&text=",
-    flitto:
-      "https://ko.flitto.com/language/translation/text?src_lang_id=17&dst_lang_id=33&lang_id=33&q=",
-    samsung: "https://translate.samsung.com/?text=",
-    deepl: "https://www.deepl.com/translator#auto/ko/",
-  };
-
-  const andEscape = {
-    // & 문자에 특수 처리해야 하는 사이트
-    papago: "%25amp",
-  };
-
   whale.storage.sync.get(
     {
       Service: "papago",
       Copy: 0,
     },
     (items) => {
-      let q = arg ? arg : getSelectionText();
+      const q = arg || getSelectionText();
+      const link = escapeURL(items.Service, q);
       if (items.Copy) copy(q);
-      let service = items.Service;
-      openSidebar(url[service], q, andEscape[service]);
+      openSidebar(link);
     }
   );
 };
